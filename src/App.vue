ffded2d54b6a95885e9d2a6b31a375bc7c1ffe10
rochabianca <bianca.rocha1711@gmail.com>
2020-08-26 09:37:26 -0300

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="page lists-show">
+      <div class="wrapper-message">
+        <span class="icon-face-sad"></span>
+        <div class="title-message">Oh no!</div>
+        <div class="subtitle-message">Something went wrong</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "App"
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/css/index.css";
 </style>
